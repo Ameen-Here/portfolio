5a21fd39530b9da89b438a0a684618a243fbdcd6
Ameen-Here <johnharry125@yahoo.com>
2022-08-30 18:22:35 +0530

    Added dark/bright theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
+  const [themeClass, setThemeClass] = useState("");
+
+  const themeHandler = () => {
+    if (themeClass === "") {
+      setThemeClass("dark_theme");
+    } else {
+      setThemeClass("");
+    }
+  };
   return (
-    <>
-      <Header />
+    <div className={themeClass}>
+      <Header themeSwitch={themeHandler} />
       <main className="main">
         <Home />
+        <About />
       </main>
-    </>
+    </div>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 import "./Header.css";
 
-const Header = () => {
+const Header = (props) => {
   /* ===================  TOGGLE MENU =================== */
   const [Toggle, showMenu] = useState(false);
+  const [themeIcon, setThemeIcon] = useState(<i class="uil uil-moon"></i>);
+
+  const [curTheme, setCurTheme] = useState("Bright");
+  const switchTheme = () => {
+    props.themeSwitch();
+    if (curTheme === "Bright") {
+      setCurTheme("Dark");
+      setThemeIcon(<i class="uil uil-brightness"></i>);
+    } else {
+      setCurTheme("Bright");
+      setThemeIcon(<i class="uil uil-moon"></i>);
+    }
+  };
 
   return (
     <header className="header">
@@ -41,6 +54,11 @@ const Header = () => {
             <li className="nav__item">
               <a href="#contact" className="nav__link">
                 <i className="uil uil-message nav__icon"></i>Contact
+              </a>
+            </li>
+            <li className="nav__item">
+              <a onClick={switchTheme} className="nav__link nav__theme_switch">
+                {themeIcon}
               </a>
             </li>
           </ul>

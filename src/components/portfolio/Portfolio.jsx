5577db5e7646601a4af7ab2350d10b470b6b91c2
Ameen-Here <johnharry125@yahoo.com>
2022-08-31 20:37:08 +0530

    Dynamically adding feature datas

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import "./Portfolio.css";
-import WorkImg from "../../assets/work1.jpg";
 import ReactPlayer from "react-player";
 
-import React from "react";
+import React, { useState } from "react";
+import { featuredData } from "./featuredData";
 
 const Portfolio = () => {
+  const [showContent, setShowContent] = useState(featuredData);
   return (
     <section className="section portfolio" id="portfolio">
       <h2 className="section__title">Portfolio</h2>
@@ -21,115 +22,62 @@ const Portfolio = () => {
       </div>
 
       <div className="portfolio__lists container grid">
-        <div className="portfolio__list">
-          <div className="portfolio__left">
-            <ReactPlayer
-              className="portfolio__video"
-              width="100%"
-              height="100%"
-              url="https://youtu.be/joKw1EtBVhw"
-              controls={true}
-              light={true}
-            />
+        {showContent.map((itemData) => {
+          return (
+            <div className="portfolio__list">
+              <div className="portfolio__left">
+                <ReactPlayer
+                  className="portfolio__video"
+                  width="100%"
+                  height="100%"
+                  url={itemData.url}
+                  controls={true}
+                  light={true}
+                />
 
-            <div className="portfolio__links">
-              <a
-                href="https://github.com/Ameen-Here/Asset-Tracker-Website"
-                target="_blank"
-                className="portfolio__link"
-              >
-                Code <i class="contact__button-icon bx bxl-github"></i>
-              </a>
-              <a
-                href="https://fierce-citadel-85684.herokuapp.com/"
-                target="_blank"
-                className="portfolio__link"
-              >
-                Live <i class="contact__button-icon bx bxs-window-alt"></i>
-              </a>
+                <div className="portfolio__links">
+                  <a
+                    href={itemData.gitUrl}
+                    target="_blank"
+                    className="portfolio__link"
+                  >
+                    Code <i class="contact__button-icon bx bxl-github"></i>
+                  </a>
+                  <a
+                    href={itemData.liveUrl}
+                    target="_blank"
+                    className="portfolio__link"
+                  >
+                    Live <i class="contact__button-icon bx bxs-window-alt"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="portfolio__description">
+                <h2 className="portfolio__header">{itemData.mainHeader}</h2>
+                <p className="portfolio__sub-description">
+                  {itemData.mainContent}
+                </p>
+
+                {itemData.probDescription && (
+                  <>
+                    <h2 className="portfolio__header">
+                      What problem it solves
+                    </h2>
+                    <p className="portfolio__sub-description">
+                      {itemData.probDescription}
+                    </p>
+                  </>
+                )}
+
+                <h2 className="portfolio__header">Features</h2>
+                <p className="portfolio__sub-description">
+                  {itemData.featDescription}
+                </p>
+              </div>
             </div>
-          </div>
-
-          <div className="portfolio__description">
-            <h2 className="portfolio__header">Asset Tracker</h2>
-            <p className="portfolio__sub-description">
-              Asset Tracker helps you track all your financial assets. In this
-              website, the users will get live day to day update of their stocks
-              as well as they can track their personal assets.
-            </p>
-
-            <h2 className="portfolio__header">What problem it solves</h2>
-            <p className="portfolio__sub-description">
-              Users buy assets from different platforms available in the market,
-              to track these assets manually as well as to calculate profit/loss
-              is a time consuming task. Asset Tracker solves this problem
-            </p>
-
-            <h2 className="portfolio__header">Features</h2>
-            <p className="portfolio__sub-description">
-              Live update every 24 hours <span className="bulletin">*</span>{" "}
-              Graphical representation using charts{" "}
-              <span className="bulletin">*</span> Authentication and secure
-              datas <span className="bulletin">*</span> Local assets with update
-              price feature
-            </p>
-          </div>
-        </div>
-
-        <div className="portfolio__list">
-          <div className="portfolio__left">
-            <ReactPlayer
-              className="portfolio__video"
-              width="100%"
-              height="100%"
-              url="https://youtu.be/TRG5cUcJsx0"
-              controls={true}
-              light={true}
-            />
-            <div className="portfolio__links">
-              <a
-                href="https://github.com/Ameen-Here/Review-Generator-API"
-                target="_blank"
-                className="portfolio__link"
-              >
-                Code <i class="contact__button-icon bx bxl-github"></i>
-              </a>
-              <a
-                href="https://reviewgenerator-16007.web.app/"
-                target="_blank"
-                className="portfolio__link"
-              >
-                Live <i class="contact__button-icon bx bxs-window-alt"></i>
-              </a>
-            </div>
-          </div>
-
-          <div className="portfolio__description">
-            <h2 className="portfolio__header">Review Generator API</h2>
-            <p className="portfolio__sub-description">
-              Review Generator API generates random testimonials for your
-              application. 1000's random of reviews with 1000's of unique names
-              from different parts of the world and random rating system, all
-              customisable by user.
-            </p>
-
-            <h2 className="portfolio__header">What problem it solves</h2>
-            <p className="portfolio__sub-description">
-              TIred of copy and pasting lorem for user's testimonial text.It
-              solves this issue by creating random reviews for your mock/demo
-              shopping website, product pages, etc.
-            </p>
-
-            <h2 className="portfolio__header">Features</h2>
-            <p className="portfolio__sub-description">
-              API limit update every 24 hours{" "}
-              <span className="bulletin">*</span> 1000's of unique reviews{" "}
-              <span className="bulletin">*</span> 1000's of unique names from
-              different parts of the world <span className="bulletin">*</span>{" "}
-              Responsive and interactive documentation page.
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/components/portfolio/featuredData.js
@@ -0,0 +1,31 @@
+export const featuredData = [
+  {
+    url: "https://youtu.be/joKw1EtBVhw",
+    gitUrl: "https://github.com/Ameen-Here/Asset-Tracker-Website",
+    liveUrl: "https://fierce-citadel-85684.herokuapp.com/",
+    mainHeader: "Asset Tracker",
+    mainContent: `Asset Tracker helps you track all your financial assets. In this
+    website, the users will get live day to day update of their stocks
+    as well as they can track their personal assets.`,
+    probDescription: `    Users buy assets from different platforms available in the market,
+    to track these assets manually as well as to calculate profit/loss
+    is a time consuming task. Asset Tracker solves this problem`,
+    featDescription: `  Live update every 24 hours | Graphical representation using charts | Authentication and secure
+    datas | Local assets with update | price feature`,
+  },
+  {
+    url: "https://youtu.be/TRG5cUcJsx0",
+    gitUrl: "https://github.com/Ameen-Here/Review-Generator-API",
+    liveUrl: "https://reviewgenerator-16007.web.app/",
+    mainHeader: "Review Generator API",
+    mainContent: `Review Generator API generates random testimonials for your
+    application. 1000's random of reviews with 1000's of unique names
+    from different parts of the world and random rating system, all
+    customisable by user.`,
+    probDescription: ` Tired of copy and pasting lorem for user's testimonial text.It
+    solves this issue by creating random reviews for your mock/demo
+    shopping website, product pages, etc.`,
+    featDescription: `API limit update every 24 hours | 1000's of unique reviews | 1000's of unique names from
+    different parts of the world | Responsive and interactive documentation page.`,
+  },
+];
